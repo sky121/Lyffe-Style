f21ad7d1aadb46aa85be680c2dea9c642e1ecd8b
Skylar Hoffman <skylarooz@gmail.com>
2020-02-01 15:15:22 -0800

    Add files via upload

--- Text Extension/content.js
+++ Text Extension/content.js
@@ -1,6 +1,5 @@
-// Inform the background page that
-// this tab should have a page-action.
-var style = "https://upload.wikimedia.org/wikipedia/en/thumb/1/1c/Pablo_Picasso%2C_1910%2C_Girl_with_a_Mandolin_%28Fanny_Tellier%29%2C_oil_on_canvas%2C_100.3_x_73.6_cm%2C_Museum_of_Modern_Art_New_York..jpg/555px-Pablo_Picasso%2C_1910%2C_Girl_with_a_Mandolin_%28Fanny_Tellier%29%2C_oil_on_canvas%2C_100.3_x_73.6_cm%2C_Museum_of_Modern_Art_New_York..jpg";
+var style =
+  "https://upload.wikimedia.org/wikipedia/en/thumb/1/1c/Pablo_Picasso%2C_1910%2C_Girl_with_a_Mandolin_%28Fanny_Tellier%29%2C_oil_on_canvas%2C_100.3_x_73.6_cm%2C_Museum_of_Modern_Art_New_York..jpg/555px-Pablo_Picasso%2C_1910%2C_Girl_with_a_Mandolin_%28Fanny_Tellier%29%2C_oil_on_canvas%2C_100.3_x_73.6_cm%2C_Museum_of_Modern_Art_New_York..jpg";
 
 chrome.runtime.sendMessage({
   from: "content",
@@ -15,40 +14,38 @@ chrome.runtime.onMessage.addListener((msg, sender, response) => {
     //  should be equivalent to jquery's `$(...)`.)
     let imgs = document.querySelectorAll("img");
     //for (imgElt of imgs[0:2]) {
-    setTimeout(myFunction(imgs[0],style),50)
+    setTimeout(myFunction(imgs[0], style), 50);
     //}
   }
 });
 
-function myFunction(image, style){
+function myFunction(image, style) {
+  // Creating a XHR object
+  let xhr = new XMLHttpRequest();
+  let url = "http://127.0.0.1:5000/postmethod";
 
-    // Creating a XHR object
-    let xhr = new XMLHttpRequest();
-    let url = "http://127.0.0.1:5000/postmethod";
+  // open a connection
+  xhr.open("POST", url, true);
+  xhr.responseType = "blob";
 
-    // open a connection
-    xhr.open("POST", url, true);
-    xhr.responseType = "blob";
+  // Set the request header i.e. which type of content you are sending
+  xhr.setRequestHeader("Content-Type", "application/json");
 
-    // Set the request header i.e. which type of content you are sending
-    xhr.setRequestHeader("Content-Type", "application/json");
+  // Create a state change callback
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState === 4 && xhr.status === 200) {
+      // Print received data from server
+      let resultImg = this.response;
+      let url = URL.createObjectURL(resultImg);
+      image.src = url;
+    }
+  };
 
-    // Create a state change callback
-    xhr.onreadystatechange = function () {
-        if (xhr.readyState === 4 && xhr.status === 200) {
+  // Converting JSON data to string
+  var data = JSON.stringify({ content: image.src, style: style });
 
-            // Print received data from server
-            let resultImg = this.response;
-            let url = URL.createObjectURL(resultImg);
-            image.src = url;
-        }
-    };
+  // Sending data with the request
+  xhr.send(data);
 
-    // Converting JSON data to string
-    var data = JSON.stringify({ "content": image.src, "style": style });
-
-    // Sending data with the request
-    xhr.send(data);
-
-    return url
+  return url;
 }
