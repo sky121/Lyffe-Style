1a163c5116bdbae032dd313de60843c076ebf9e8
jacobjh1 <60554654+jacobjh1@users.noreply.github.com>
2020-02-01 16:58:25 -0800

    Add files via upload

--- Jacob/content.js
+++ Jacob/content.js
@@ -1,57 +1,53 @@
-// Inform the background page that
-// this tab should have a page-action.
-chrome.runtime.sendMessage({
-  from: "content",
-  subject: "showPageAction"
-});
+
+
+// chrome.runtime.sendMessage({
+//   from: "content",
+//   subject: "showPageAction"
+// });
 
 // Listen for messages from the popup.
-chrome.runtime.onMessage.addListener((msg, sender, response) => {
-//chrome.runtime.onMessage.addListener((sender, msg, response) => {
+chrome.runtime.onMessage.addListener(function(msg, sender, response) {
   // First, validate the message's structure.
-
-  console.log("DOM content");
-
   if (msg.from === "popup" && msg.subject === "DOMInfo") {
+    var style = msg.file;
     // Collect the necessary data.
     // (For your specific requirements `document.querySelectorAll(...)`
     //  should be equivalent to jquery's `$(...)`.)
-
-    var domInfo = {
-      total: document.querySelectorAll("*").length,
-      inputs: document.querySelectorAll("input").length,
-      buttons: document.querySelectorAll("button").length
-    };
-
-    // Directly respond to the sender (popup),
-    // through the specified callback.
-    response(domInfo);
-  }
-
-  else if (msg.from === "popup" && msg.subject === "apply") {    
-    console.log("apply_content");
+    console.log(style);
     let imgs = document.querySelectorAll("img");
-
-    for (imgElt of imgs) {
-      let file = "images/kitten.jpg";
-      let url = chrome.runtime.getURL(file);
-      imgElt.src = url;
-    }
-
-    var imgInfo = {
-      done: true
-    }
-    response(imgInfo);
+    //for (imgElt of imgs[0:2]) {
+    setTimeout(myFunction(imgs[0], style), 50);
+    //}
   }
-
-
-  else if (msg.from === "popup" && msg.subject === "submit") {
-    let file = msg.file;
-    console.log(file.src, file.alt);
-  }
-
-  // doesn't work :c
-  return Promise.resolve("Dummy response to keep the console quiet");
-
-
 });
+
+function myFunction(image, style) {
+  // Creating a XHR object
+  let xhr = new XMLHttpRequest();
+  let url = "http://127.0.0.1:5000/postmethod";
+
+  // open a connection
+  xhr.open("POST", url, true);
+  xhr.responseType = "blob";
+
+  // Set the request header i.e. which type of content you are sending
+  xhr.setRequestHeader("Content-Type", "application/json");
+
+  // Create a state change callback
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState === 4 && xhr.status === 200) {
+      // Print received data from server
+      let resultImg = this.response;
+      let url = URL.createObjectURL(resultImg);
+      image.src = url;
+    }
+  };
+
+  // Converting JSON data to string
+  var data = JSON.stringify({ content: image.src, style: style });
+
+  // Sending data with the request
+  xhr.send(data);
+
+  return url;
+}
