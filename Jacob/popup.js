10629ba57f251d2a96731e560d0070465aa9fce2
jacobjh1 <60554654+jacobjh1@users.noreply.github.com>
2020-02-01 16:45:31 -0800

    Add files via upload

--- Jacob/popup.js
+++ Jacob/popup.js
@@ -1,20 +1,31 @@
 // Update the relevant fields with the new data.
-const setDOMInfo = info => {
-  document.getElementById("total").textContent = info.total;
-  document.getElementById("inputs").textContent = info.inputs;
-  document.getElementById("buttons").textContent = info.buttons;
-};
-
-const setApply = info => {
-  console.log('apply');
-};
-
-const setSubmit = info => {
-  console.log('submit');
-};
-
 window.addEventListener("DOMContentLoaded", () => {
   // ...query for the active tab...
+
+  var input_files = document.getElementById("files");
+  function select(event) {
+    console.log('sel_file');
+    var file = event.target.files[0];
+    if (file.type.match("image.*")) {
+      document.getElementById("submit_image").innerHTML =
+        '<img src = "' +
+        window.URL.createObjectURL(file) +
+        '" alt = "object file" id = "chosen_file">';
+    }
+    stylish({
+      from: "popup",
+      subject: "DOMInfo",
+      file: document.getElementById("chosen_file") ? 
+            document.getElementById("chosen_file").src :
+            "https://upload.wikimedia.org/wikipedia/en/thumb/1/1c/Pablo_Picasso%2C_1910%2C_Girl_with_a_Mandolin_%28Fanny_Tellier%29%2C_oil_on_canvas%2C_100.3_x_73.6_cm%2C_Museum_of_Modern_Art_New_York..jpg/555px-Pablo_Picasso%2C_1910%2C_Girl_with_a_Mandolin_%28Fanny_Tellier%29%2C_oil_on_canvas%2C_100.3_x_73.6_cm%2C_Museum_of_Modern_Art_New_York..jpg"
+    });
+  }
+  input_files.addEventListener("change", select);
+  
+
+
+
+
   chrome.tabs.query(
     {
       active: true,
@@ -22,70 +33,98 @@ window.addEventListener("DOMContentLoaded", () => {
     },
     tabs => {
       // ...and send a request for the DOM info...
-      chrome.tabs.sendMessage(
-        tabs[0].id,
-        { from: "popup", subject: "DOMInfo" },
-        // ...also specifying a callback to be called
-        //    from the receiving end (content script).
-        setDOMInfo
+      console.log('setup');
+      chrome.tabs.sendMessage(tabs[0].id, {
+        from: "popup",
+        subject: "DOMInfo",
+        file: document.getElementById("chosen_file") ? 
+              document.getElementById("chosen_file").src :
+              "https://upload.wikimedia.org/wikipedia/en/thumb/1/1c/Pablo_Picasso%2C_1910%2C_Girl_with_a_Mandolin_%28Fanny_Tellier%29%2C_oil_on_canvas%2C_100.3_x_73.6_cm%2C_Museum_of_Modern_Art_New_York..jpg/555px-Pablo_Picasso%2C_1910%2C_Girl_with_a_Mandolin_%28Fanny_Tellier%29%2C_oil_on_canvas%2C_100.3_x_73.6_cm%2C_Museum_of_Modern_Art_New_York..jpg"
+        }
       );
     }
   );
 
-
-
-  const apply_button = document.getElementById('button_apply');
-
-  // apply_button.addEventListener("click", () => {         // ) needed at end before ;
-  apply_button.onclick = applyButton;
-    
-  function applyButton() {
-    chrome.tabs.query(
-      { active: true, currentWindow: true },
-      tabs => {
-        chrome.tabs.sendMessage(
-          tabs[0].id,
-          { from: "popup", subject: "apply"},
-           // callback here, do something with the file:
-           setApply
-        );
-      } 
-    );
-  };
-
-
-
-  var input_files = document.getElementById('files');
-
-  function select (event) {
-      var file = event.target.files[0];
-      if (file.type.match("image.*")) {
-        var reader = new FileReader();
-        //reader.onload = (function(aImg) { return function(e) { aImg.src = e.target.result; }; })(img);
-        document.getElementById("submit_image").innerHTML = '<button type = "button"' +
-            'id = "button_submit">Submit</button>' + 
-            '<img src = "' + window.URL.createObjectURL(file) +
-            '" alt = "object file" id = "chosen_file">';
-      }
-  }
-
-  input_files.addEventListener("change", select);
-
-  var submit_file = document.getElementById("button_submit");
-  submit_file.onclick = submit;
-  function submit() {
-    chrome.tabs.query(
-      { active: true, currentWindow: true },
-      tabs => {
-        chrome.tabs.sendMessage(
-          tabs[0].id,
-          { from: "popup", subject: "submit", 
-            file : document.getElementById("chosen_file")},
-           // callback here, do something with the file:
-           setSubmit
-        );
-      } 
-    );
-  };
-
 });
+
+// DOMAttrModified
+// DOMSubtreeModified
+// window.addEventListener("DOMSubtreeModified", () => {
+//   chrome.tabs.query(
+//     {
+//       active: true,
+//       currentWindow: true
+//     },
+//     tabs => {
+//       console.log('next');
+//       // ...and send a request for the DOM info...
+//       chrome.tabs.sendMessage(tabs[0].id, {
+//         from: "popup",
+//         subject: "DOMInfo",
+//         file: document.getElementById("chosen_file").src
+//       });
+//     }
+//   );
+
+// });
+
+
+
+
+
+
+// var style =
+//   "https://upload.wikimedia.org/wikipedia/en/thumb/1/1c/Pablo_Picasso%2C_1910%2C_Girl_with_a_Mandolin_%28Fanny_Tellier%29%2C_oil_on_canvas%2C_100.3_x_73.6_cm%2C_Museum_of_Modern_Art_New_York..jpg/555px-Pablo_Picasso%2C_1910%2C_Girl_with_a_Mandolin_%28Fanny_Tellier%29%2C_oil_on_canvas%2C_100.3_x_73.6_cm%2C_Museum_of_Modern_Art_New_York..jpg";
+
+// chrome.runtime.sendMessage({
+//   from: "content",
+//   subject: "showPageAction"
+// });
+// Listen for messages from the popup.
+// chrome.runtime.onMessage.addListener((msg, sender, response) => {
+  // First, validate the message's structure.
+
+function stylish (msg) {
+  if (msg.from === "popup" && msg.subject === "DOMInfo") {
+    // Collect the necessary data.
+    // (For your specific requirements `document.querySelectorAll(...)`
+    //  should be equivalent to jquery's `$(...)`.)
+    style = msg.file;
+    console.log(style);
+    let imgs = document.querySelectorAll("img");
+    //for (imgElt of imgs[0:2]) {
+    setTimeout(myFunction(imgs[0], style), 50);
+    //}
+  }
+}
+
+function myFunction(image, style) {
+  // Creating a XHR object
+  let xhr = new XMLHttpRequest();
+  let url = "http://127.0.0.1:5000/postmethod";
+
+  // open a connection
+  xhr.open("POST", url, true);
+  xhr.responseType = "blob";
+
+  // Set the request header i.e. which type of content you are sending
+  xhr.setRequestHeader("Content-Type", "application/json");
+
+  // Create a state change callback
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState === 4 && xhr.status === 200) {
+      // Print received data from server
+      let resultImg = this.response;
+      let url = URL.createObjectURL(resultImg);
+      image.src = url;
+    }
+  };
+
+  // Converting JSON data to string
+  var data = JSON.stringify({ content: image.src, style: style });
+
+  // Sending data with the request
+  xhr.send(data);
+
+  return url;
+}
